5b126cd4af58e073582ce608724d4847075f1be7
Ramsey Nasser <ram@nas.sr>
2012-12-29 14:45:05 -0500

    Remove /files

--- app.rb
+++ app.rb
@@ -4,13 +4,3 @@ get '/' do
   content_type :html, 'charset' => 'utf-8'
   send_file "public/repl.html"
 end
-
-get '/files' do
-  s = ""
-
-  Dir["**/*"].each do |f|
-    s += "#{f}\n"
-  end
-
-  s
-end
