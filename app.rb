e1b5f4a21627e12ba4e702bac14b3e2133b0e16c
Ramsey Nasser <ram@nas.sr>
2012-12-29 12:34:52 -0500

    Fix codemirror.js not showing up on Heroku bug

--- app.rb
+++ app.rb
@@ -1,9 +1,5 @@
 require "sinatra"
 
-set :protection, :except => :frame_options
-set :static, true
-set :root, File.dirname(__FILE__)
-
 get '/' do
   content_type :html, 'charset' => 'utf-8'
   send_file "public/repl.html"
