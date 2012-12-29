13c29e583281c6777a02d7a3737bc0aa8349aade
Ramsey Nasser <ram@nas.sr>
2012-12-29 14:18:01 -0500

    Add /files to debug Heroku deployment

--- app.rb
+++ app.rb
@@ -4,3 +4,13 @@ get '/' do
   content_type :html, 'charset' => 'utf-8'
   send_file "public/repl.html"
 end
+
+get '/files' do
+  s = ""
+
+  Dir["**/*"].each do |f|
+    s += "#{f}\n"
+  end
+
+  s
+end
