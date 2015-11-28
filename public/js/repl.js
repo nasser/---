51bd20b0fb792d1a03e272162ccf52088428c0e9
Ramsey Nasser <ram@nas.sr>
2015-11-28 16:28:04 -0500

    Make standalone build

--- public/js/repl.js
+++ public/js/repl.js
@@ -1,3 +1,9 @@
+var snippetList = document.querySelectorAll("script[type='text/قلب']");
+var snippets = {}
+for(var i=0; i<snippetList.length; i++) {
+  snippets[snippetList[i].getAttribute("name")] = snippetList[i].innerText;
+}
+
 function arabize (str) {
   if(typeof str == 'number') {
     return str.toString().
@@ -29,101 +35,29 @@ function arabize (str) {
 // Instantiate the console widget.
 var jqconsole = $('#console').jqconsole('', '>>> ');
 jqconsole.RegisterMatching('(', ')', 'parans');
-jqconsole.RegisterShortcut('C', function() {
-  this.AbortPrompt();
-  startPrompt();
-}); 
-jqconsole.RegisterShortcut('A', function() {
-  this.MoveToStart();
-}); 
-jqconsole.RegisterShortcut('E', function() {
-  this.MoveToEnd();
-});
+
+console.log = function(str) {
+  jqconsole.Append("<span class='jqconsole-output'>" + arabize(str) + "\n</span>");
+}
+
+console.warn = function(str) {
+  jqconsole.Append("<span class='jqconsole-warn'>" + arabize(str) + "\n</span>");
+}
 
 // Starts a REPL prompt.
 function startPrompt () {
   jqconsole.Prompt(true, function (str) {
     // Main Enter callback.
     str = str.trim();
-    if (str) {
-      interpreter.execute(str);
-    } else {
-      startPrompt();
-    }
-  }, function (str, callback) {
-    // Line continuation callback.
-    // Note that It's async because the qlb is in a worker.
-    interpreter.isLineEnd(str, callback);
-  }, true);
+    var result = Qlb.execute(str)
+    
+    if(result != undefined)
+      console.log("==> " + result);
+    
+    startPrompt();
+  });
 }
 
-// Create the worker with the worker adapter.
-var worker = new Worker('js/worker.js');
-
-// The main interpreter wrapper.
-var interpreter = {
-  // Called with result from qlb execution by worker.onmessage.
-  result: function (str) {
-    if (str && (str.length !== undefined ? str.length > 0 : true)) {
-      jqconsole.Write('==> ' + arabize(str) + '\n', 'jqconsole-output');
-    }
-    // Restart the prompt.
-    startPrompt();
-  },
-  // Called with prints from qlb by worker.onmessage.
-  log: function (str) {
-    jqconsole.Write(arabize(str) + '\n', 'jqconsole-output', false);
-  },
-  // Called with erros from qlb execution by worker.onmessage.
-  warn: function (str) {
-    jqconsole.Write('\n' + str + '\n\n', 'jqconsole-warn', false);
-    // Restart the prompt.
-    startPrompt();
-  },
-  // Sends an execute command to the worker.
-  execute: function (str) {
-    worker.postMessage({
-      type: 'execute',
-      data: str
-    });
-  },
-  // Asks the worker if the command at hand is done or needs more lines.
-  isLineEnd: function (str, callback) {
-    // When done the worker will send a message with type `isLineEndResult`
-    // attatch the callback to that.
-    interpreter.isLineEndResult = callback;
-    worker.postMessage({
-      type: 'isLineEnd',
-      data: str
-    });
-  }
-};
-
-// Worker message router.
-worker.onmessage = function (event) {
-  var type = event.data.type,
-      data = event.data.data;
-  if (interpreter[type]) {
-    interpreter[type](data);
-  }
-};
-
-// Delegate events from examples etc.
-$(document).on('click', 'a.execute', function () {
-  jqconsole.SetPromptText('(' + decodeURI(this.href).match(/#(.*)/)[1] + ')');
-  jqconsole._HandleEnter();
-});
-
-$(document).on('click', 'a.load', function () {
-  var url = this.href.match(/#(.*)/)[1];
-  $.get("/lib/" + url + ".qlb", function(code) {
-    jqconsole.SetPromptText(code);
-  });
-});
-
-// Go!
-interpreter.execute('(ضمن "mtfaail/mtfaail")');
-interpreter.execute('(ضمن-تمديد "rasm/rasm")')
 startPrompt();
 
 function hl() {
@@ -131,9 +65,48 @@ function hl() {
     // .add($(".jqconsole-prompt > span").eq(0))
     // .add($(".jqconsole-prompt .jqconsole-cursor").prev())
     .each(function(i, e) {
-      Rainbow.color(this.innerText, "qlb", function(c) { $(e).html(c) } )
+      if(this.innerText)
+        Rainbow.color(this.innerText, "qlb", function(c) { $(e).html(c) } )
     });
+}
+
+window.onhashchange = function(e) {
+  console.log(e.newURL);
 }
 
 setInterval(hl, 1000);
 
+Qlb.execute(snippets.البداية)
+
+function codeLink() {
+  var code = event.target.getAttribute("code");
+  jqconsole.Append("<span class='jqconsole-old-prompt'>&gt;&gt;&gt; " + code + "\n</span>");
+  Qlb.execute(code);
+}
+
+function loadSnippet() {
+  var snippet = event.target.getAttribute("name");
+  var code = snippets[snippet].trim();
+  var lines = code.split("\n");
+  for (var i = 0; i < lines.length; i++) {
+    jqconsole.Append("<span class='jqconsole-old-prompt'>..." + lines[i] + "\n</span>");
+  };
+  
+  var result = Qlb.execute(code);
+  if(result != undefined)
+    console.log("==> " + arabize(result));
+}
+
+Qlb.globalEnvironment.merge({
+  "حمل": function(snippet) {
+    code = snippets[snippet].trim();
+    lines = code.split("\n");
+    for (var i = 0; i < lines.length; i++) {
+      jqconsole.Append("<span class='jqconsole-old-prompt'>..." + lines[i] + "\n</span>");
+    };
+    
+    var result = Qlb.execute(code);
+    if(result != undefined)
+      console.log("==> " + arabize(result));
+  }
+});
